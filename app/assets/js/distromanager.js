db57c64272e96e82e52fe3180af5c7cdd4b9d129
ppaka <asj0216@gmail.com>
2024-09-26 03:19:26 +0900

    Change Distro URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://cdn.mysticred.space/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://193.123.253.36:8080/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
